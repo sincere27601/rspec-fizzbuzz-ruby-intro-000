ef5c5feb342ba17a3fafc0fed9b297c1b72c579c
Michael Raysor <michaelraysor1@gmail.com>
2021-03-18 23:39:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,22 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+def "fizzbuzz" do
+  if it 'returns "Fizz" when the number is divisible by 3' do
+    fizz_3 = fizzbuzz(3)
 
-end
+    expect(fizz_3).to eq("Fizz")
+  elsif it 'returns "Buzz" when the number is divisible by 5' do
+    buzz_5 = fizzbuzz(5)
+
+    expect(buzz_5).to eq("Buzz")
+  elsif it 'returns "FizzBuzz" when the is divisible by 15' do
+    fizzbuzz_15 = fizzbuzz(15)
+
+    expect(fizzbuzz_15).to eq("FizzBuzz")
+  else 
+    puts "nil!"
+  end
+  end
+  end
+
+
