bfbc9b449913a5aad52b651df31f9adc495ae1b6
Michael Raysor <michaelraysor1@gmail.com>
2021-03-18 23:41:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def "fizzbuzz" do
+describe "fizzbuzz" do
   if it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
 
